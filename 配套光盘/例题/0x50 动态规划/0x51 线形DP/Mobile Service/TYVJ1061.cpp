bad58076e57655a30a3c6573c7ca4126a23deeb4
lydrainbowcat <hyqjlyd@qq.com>
2018-05-17 23:02:34 +0900

    Update std for 0x51 problem Mobile Service

--- 配套光盘/例题/0x50 动态规划/0x51 线形DP/Mobile Service/TYVJ1061.cpp
+++ 配套光盘/例题/0x50 动态规划/0x51 线形DP/Mobile Service/TYVJ1061.cpp
@@ -21,9 +21,10 @@ int main()
 				if(f[i-1&1][x][y]!=0x3f3f3f3f)
 				{
 					int z=p[i-1];
-					f[i&1][y][z]=min(f[i&1][y][z],f[i-1&1][x][y]+c[x][p[i]]);
-					f[i&1][x][z]=min(f[i&1][x][z],f[i-1&1][x][y]+c[y][p[i]]);
-					f[i&1][x][y]=min(f[i&1][x][y],f[i-1&1][x][y]+c[z][p[i]]);
+					// the problem doesn't allow two people at the same place
+					if(y!=p[i]&&z!=p[i]) f[i&1][y][z]=min(f[i&1][y][z],f[i-1&1][x][y]+c[x][p[i]]);
+					if(x!=p[i]&&z!=p[i]) f[i&1][x][z]=min(f[i&1][x][z],f[i-1&1][x][y]+c[y][p[i]]);
+					if(x!=p[i]&&y!=p[i]) f[i&1][x][y]=min(f[i&1][x][y],f[i-1&1][x][y]+c[z][p[i]]);
 					f[i-1&1][x][y]=0x3f3f3f3f;
 				}
 	int ans=1<<30;
