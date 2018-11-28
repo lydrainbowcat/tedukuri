2fbfa34a374bd6ec40c98482c73d87ac79e4a4e3
Planet6174 <ltq2002gcn@gmail.com>
2018-11-28 14:08:37 +0800

    添加 0x43. Atlantis 的测试数据

---
+++ 配套光盘/例题/0x40 数据结构进阶/0x43 线段树/Atlantis/atlantis.sol
@@ -0,0 +1,30 @@
+Test case #1
+Total explored area: 7485400000.00
+
+Test case #2
+Total explored area: 10000000000.00
+
+Test case #3
+Total explored area: 3457160000.00
+
+Test case #4
+Total explored area: 0.00
+
+Test case #5
+Total explored area: 180.00
+
+Test case #6
+Total explored area: 1.00
+
+Test case #7
+Total explored area: 2.00
+
+Test case #8
+Total explored area: 495.00
+
+Test case #9
+Total explored area: 8532.40
+
+Test case #10
+Total explored area: 0.01
+
