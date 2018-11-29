73613618372936170643153748f173106058b662
lydrainbowcat <hyqjlyd@qq.com>
2018-11-29 14:31:17 +0900

    修正 0x52背包 例题 POJ1015 标程

--- 配套光盘/例题/0x50 动态规划/0x52 背包/Jury Compromise/poj1015.cpp
+++ 配套光盘/例题/0x50 动态规划/0x52 背包/Jury Compromise/poj1015.cpp
@@ -1,74 +1,56 @@
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
 #include <algorithm>
+#include <vector>
 using namespace std;
-int f[30][810],path[30][810],a[210],b[210];
-int n,m,now;
-inline bool find(int i, int j, int k)
-{
-	if(i == 0) return false;
-	if(path[i][j] == k) return true;
-	return find(i-1,j-a[path[i][j]],k);
-}
-int main()
-{
-	int cas(0);
-	while(scanf("%d%d",&n,&m),n+m)
-	{
-		memset(f,-1,sizeof f);
-		now = 20 * m;f[0][now] = 0;
-		for(int i = 1; i <= n; i++)
-		{
-			int x,y;
-			scanf("%d%d",&x,&y);
-			a[i] = x-y;
-			b[i] = x+y;
-		}
-		for(int i = 1; i <= m; i++)
-			for(int j = 0;j <= 2 * now; j++)
-				if(~f[i-1][j])
-					for(int k = 1;k <= n; k++)
-						if(!find(i-1,j,k))
-							if(f[i][j+a[k]] < f[i-1][j] + b[k])
-							{
-								f[i][j+a[k]] = f[i-1][j] + b[k];
-								path[i][j+a[k]] = k;
-							}
+int f[25][805]; // 前i个人，选了j个，辩方总分与控方总分的差为k=-400~400（平移到0~800）
+int d[205][25][805]; // 记路径还是要三维，不然后面的更新可能会覆盖掉之前的记录
+int n, m, a[205], b[205], suma, sumb, T;
+vector<int> c;
 
-		int ans(0x7fffffff),num,sum;
-		for(int i = now;i <= 2*now;i++)
-		if(~f[m][i])
-		{
-			ans = i - now;
-			num = i;
-			sum = f[m][i];
-			break;
+void get_path(int i, int j, int k) {
+	if (j == 0) return;
+	int last = d[i][j][k];
+	get_path(last - 1, j - 1, k - (a[last] - b[last]));
+	c.push_back(last);
+	suma += a[last], sumb += b[last];
+}
+
+int main() {
+	while (cin >> n >> m && n) {
+		for (int i = 1; i <= n; i++) scanf("%d%d", &a[i], &b[i]);
+		memset(f, 0xcf, sizeof(f)); // -INF
+		f[0][400] = 0; // f[0][0]第三维平移400
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j <= m; j++) // 不选i
+				for (int k = 0; k <= 800; k++) d[i][j][k] = d[i - 1][j][k];
+			for (int j = m; j; j--) // 选i
+				for (int k = 0; k <= 800; k++) {
+					if (k - (a[i] - b[i]) < 0 || k - (a[i] - b[i]) > 800) continue; // 超出范围
+					if (f[j][k] < f[j - 1][k - (a[i] - b[i])] + a[i] + b[i]) {
+						f[j][k] = f[j - 1][k - (a[i] - b[i])] + a[i] + b[i];
+						d[i][j][k] = i;
+					}
+				}
 		}
-		for(int i = now; i >= 0; i--)
-		if(~f[m][i])
-			if(now - i < ans ||(now - i == ans && f[m][i] > sum))
-			{
-				ans = i - now;
-				num = i;
-				sum = f[m][i];
+		int ans = 0;
+		for (int k = 0; k <= 400; k++) {
+			if (f[m][400 + k] >= 0 && f[m][400 + k] >= f[m][400 - k]) {
+				ans = k + 400;
 				break;
 			}
-		printf("Jury #%d\n",++cas);
-		int x = (sum+ans)>>1,y = sum - x;
-		printf("Best jury has value %d for prosecution and value %d for defence: \n",x,y);
-		x=m,y=num;
-		int tot(0);
-		int out[210];
-		while(x)
-		{
-			out[++tot] = path[x][y];
-			y -= a[path[x][y]];
-			x--;
+			if (f[m][400 - k] >= 0) {
+				ans = 400 - k;
+				break;
+			}
 		}
-		sort(out+1,out+tot+1);
-		for(int i = 1; i <= tot; i++)
-		printf(" %d",out[i]);
-		puts("\n");
+		c.clear();
+		suma = sumb = 0;
+		get_path(n, m, ans);
+		printf("Jury #%d\n", ++T);
+		printf("Best jury has value %d for prosecution and value %d for defence:\n", suma, sumb);
+		for (int i = 0; i < c.size(); i++) printf(" %d", c[i]);
+		printf("\n\n");
 	}
-	return 0;
-} 
+}
