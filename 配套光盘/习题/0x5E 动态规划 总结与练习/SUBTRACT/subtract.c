e45ccef2fb00bb7117c70a338c50deb0f5e0e63d
Planet6174 <ltq2002gcn@gmail.com>
2018-11-29 12:46:44 +0800

    修了 0x5E. Substract 的 std

--- 配套光盘/习题/0x5E 动态规划 总结与练习/SUBTRACT/subtract.c
+++ 配套光盘/习题/0x5E 动态规划 总结与练习/SUBTRACT/subtract.c
@@ -1,163 +1,150 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Wayback Machine</title>
-<script src="//archive.org/includes/analytics.js?v=cf34f82" type="text/javascript"></script>
-<script type="text/javascript">window.addEventListener('DOMContentLoaded',function(){var v=archive_analytics.values;v.service='wb';v.server_name='wwwb-app29.us.archive.org';v.server_ms=802;archive_analytics.send_pageview({});});</script><link rel="stylesheet" type="text/css" href="/static/css/banner-styles.css?v=1542068202.0" />
-<link rel="stylesheet" type="text/css" href="/static/css/iconochive.css?v=1542068202.0" />
-<script src="/static/js/jquery-1.11.1.min.js"></script>
-</head>
-<body style="overflow:hidden;">
-<!-- BEGIN WAYBACK TOOLBAR INSERT -->
-<script type="text/javascript" src="/static/js/timestamp.js?v=1542068202.0" charset="utf-8"></script>
-<script type="text/javascript" src="/static/js/graph-calc.js?v=1542068202.0" charset="utf-8"></script>
-<script type="text/javascript" src="/static/js/auto-complete.js?v=1542068202.0" charset="utf-8"></script>
-<script type="text/javascript" src="/static/js/toolbar.js?v=1542068202.0" charset="utf-8"></script>
-<style type="text/css">
-body {
-  margin-top:0 !important;
-  padding-top:0 !important;
-  /*min-width:800px !important;*/
+
+#include <stdio.h>
+
+#define MAX_N 100
+#define MAX_VALUE 100
+#define FLAG -5
+
+int n,t;
+int numbers[MAX_N];
+int signs[MAX_N];
+
+FILE *fp;
+
+void read_input(void)
+{
+  int i;
+
+  // reading from "SUBTRACT.IN"
+
+  fp = fopen("SUBTRACT.IN","rt");
+
+  fscanf(fp,"%d %d",&n,&t);
+
+  for (i = 0;i < n;++i)
+    fscanf(fp,"%d",numbers + i);
+
+  fclose(fp);
 }
-.wb-autocomplete-suggestions {
-    text-align: left; cursor: default; border: 1px solid #ccc; border-top: 0; background: #fff; box-shadow: -1px 1px 3px rgba(0,0,0,.1);
-    position: absolute; display: none; z-index: 2147483647; max-height: 254px; overflow: hidden; overflow-y: auto; box-sizing: border-box;
+
+void solve(void)
+{
+  int i,j;
+  int sum,target,number;
+  int table[2 * MAX_VALUE * MAX_N + 1];
+
+  // calculating "sum" of all numbers and "target"
+  // first number in sequence must be plus, second must be minus
+
+  sum = 0;
+
+  for (i = 2;i < n;++i)
+    sum += numbers[i];
+
+  target = sum - (t - (numbers[0] - numbers[1]));
+
+  // dynamically generating "table", looking for target
+
+  table[0] = -1;
+
+  for (i = 1;i <= target;++i)
+    table[i] = FLAG;
+
+  for (i = 2;i < n;++i)
+  {
+    for (j = target - 1;j >= 0;--j)
+      if (table[j] != FLAG)
+      {
+        if (table[j + 2 * numbers[i]] == FLAG)
+          table[j + 2 * numbers[i]] = i;
+
+        if (table[target] != FLAG)
+          break;
+      }
+
+    if (table[target] != FLAG)
+      break;
+  }
+
+  // generating "signs", some numbers will have plus sign in front of them
+  // some numbers will have minus sign
+
+  signs[0] = 1;
+
+  signs[1] = -1;
+
+  for (i = 2;i < n;++i)
+    signs[i] = 1;
+
+  number = target;
+
+  do
+  {
+    if (!number)
+      break;
+
+    signs[table[number]] = -1;
+    number -= 2 * numbers[table[number]];
+  } while (1);
+
 }
-.wb-autocomplete-suggestion { position: relative; padding: 0 .6em; line-height: 23px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; font-size: 1.02em; color: #333; }
-.wb-autocomplete-suggestion b { font-weight: bold; }
-.wb-autocomplete-suggestion.selected { background: #f0f0f0; }
-</style>
-<div id="wm-ipp" lang="en" style="display:none;direction:ltr;">
-<div style="position:fixed;left:0;top:0;right:0;">
-<div id="wm-ipp-inside">
-  <div style="position:relative;">
-    <div id="wm-logo" style="float:left;width:130px;padding-top:10px;">
-      <a href="/web/" title="Wayback Machine home page"><img src="/static/images/toolbar/wayback-toolbar-logo.png" alt="Wayback Machine" width="110" height="39" border="0" /></a>
-    </div>
-    <div class="r" style="float:right;">
-      <div id="wm-btns" style="text-align:right;height:25px;">
-                  <div id="wm-save-snapshot-success">success</div>
-          <div id="wm-save-snapshot-fail">fail</div>
-          <a href="#"
-             onclick="__wm.saveSnapshot('http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c', '20010822230811')"
-             title="Share via My Web Archive"
-             id="wm-save-snapshot-open"
-          >
-            <span class="iconochive-web"></span>
-          </a>
-          <a href="https://archive.org/account/login.php"
-             title="Sign In"
-             id="wm-sign-in"
-          >
-            <span class="iconochive-person"></span>
-          </a>
-          <span id="wm-save-snapshot-in-progress" class="iconochive-web"></span>
-        	<a href="http://faq.web.archive.org/" title="Get some help using the Wayback Machine" style="top:-6px;"><span class="iconochive-question" style="color:rgb(87,186,244);font-size:160%;"></span></a>
-	<a id="wm-tb-close" href="#close" onclick="__wm.h(event);return false;" style="top:-2px;" title="Close the toolbar"><span class="iconochive-remove-circle" style="color:#888888;font-size:240%;"></span></a>
-      </div>
-      <div id="wm-share" style="text-align:right;">
-	<a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=https://web.archive.org/web/20010822230811/http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c', '', 'height=400,width=600'); return false;" title="Share on Facebook" style="margin-right:5px;" target="_blank"><span class="iconochive-facebook" style="color:#3b5998;font-size:160%;"></span></a>
-	<a href="#" onclick="window.open('https://twitter.com/intent/tweet?text=https://web.archive.org/web/20010822230811/http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c&amp;via=internetarchive', '', 'height=400,width=600'); return false;" title="Share on Twitter" style="margin-right:5px;" target="_blank"><span class="iconochive-twitter" style="color:#1dcaff;font-size:160%;"></span></a>
-      </div>
-    </div>
-    <table class="c" style="">
-      <tbody>
-	<tr>
-	  <td class="u" colspan="2">
-	    <form target="_top" method="get" action="/web/submit" name="wmtb" id="wmtb"><input type="text" name="url" id="wmtbURL" value="http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c" onfocus="this.focus();this.select();" /><input type="hidden" name="type" value="replay" /><input type="hidden" name="date" value="20010822230811" /><input type="submit" value="Go" /></form>
-	  </td>
-	  <td class="n" rowspan="2" style="width:110px;">
-	    <table>
-	      <tbody>
-		<!-- NEXT/PREV MONTH NAV AND MONTH INDICATOR -->
-		<tr class="m">
-		  <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/19990826172218/http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c" title="26 Aug 1999"><strong>Aug</strong></a></td>
-		  <td class="c" id="displayMonthEl" title="You are here: 23:08:11 Aug 22, 2001">AUG</td>
-		  <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20020703141549/http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c" title="03 Jul 2002"><strong>Jul</strong></a></td>
-		</tr>
-		<!-- NEXT/PREV CAPTURE NAV AND DAY OF MONTH INDICATOR -->
-		<tr class="d">
-		  <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/19990826172218/http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c" title="17:22:18 Aug 26, 1999"><img src="/static/images/toolbar/wm_tb_prv_on.png" alt="Previous capture" width="14" height="16" border="0" /></a></td>
-		  <td class="c" id="displayDayEl" style="width:34px;font-size:24px;white-space:nowrap;" title="You are here: 23:08:11 Aug 22, 2001">22</td>
-		  <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20020703141549/http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c" title="14:15:49 Jul 03, 2002"><img src="/static/images/toolbar/wm_tb_nxt_on.png" alt="Next capture" width="14" height="16" border="0" /></a></td>
-		</tr>
-		<!-- NEXT/PREV YEAR NAV AND YEAR INDICATOR -->
-		<tr class="y">
-		  <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/19990826172218/http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c" title="26 Aug 1999"><strong>1999</strong></a></td>
-		  <td class="c" id="displayYearEl" title="You are here: 23:08:11 Aug 22, 2001">2001</td>
-		  <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20020830121405/http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c" title="30 Aug 2002"><strong>2002</strong></a></td>
-		</tr>
-	      </tbody>
-	    </table>
-	  </td>
-	</tr>
-	<tr>
-	  <td class="s">
-	    	    <div id="wm-nav-captures">
-	      	      <a class="t" href="/web/20010822230811*/http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c" title="See a list of every capture for this URL">21 captures</a>
-	      <div class="r" title="Timespan for captures of this URL">26 Aug 1999 - 27 Sep 2006</div>
-	      </div>
-	  </td>
-	  <td class="k">
-	    <a href="" id="wm-graph-anchor">
-	      <div id="wm-ipp-sparkline" title="Explore captures for this URL" style="position: relative">
-		<canvas id="wm-sparkline-canvas" width="575" height="27" border="0"></canvas>
-	      </div>
-	    </a>
-	  </td>
-	</tr>
-      </tbody>
-    </table>
-    <div style="position:absolute;bottom:0;right:2px;text-align:right;">
-      <a id="wm-expand" class="wm-btn wm-closed" href="#expand" onclick="__wm.ex(event);return false;"><span id="wm-expand-icon" class="iconochive-down-solid"></span> <span style="font-size:80%">About this capture</span></a>
-    </div>
-  </div>
-    <div id="wm-capinfo" style="border-top:1px solid #777;display:none; overflow: hidden">
-            <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center">COLLECTED BY</div>
-    <div style="padding:3px;position:relative" id="wm-collected-by-content">
-            <div style="display:inline-block;vertical-align:top;width:50%;">
-			<span class="c-logo" style="background-image:url(https://archive.org/services/img/alexacrawls);"></span>
-		Organization: <a style="color:#33f;" href="https://archive.org/details/alexacrawls" target="_new"><span class="wm-title">Alexa Crawls</span></a>
-		<div style="max-height:75px;overflow:hidden;position:relative;">
-	  <div style="position:absolute;top:0;left:0;width:100%;height:75px;background:linear-gradient(to bottom,rgba(255,255,255,0) 0%,rgba(255,255,255,0) 90%,rgba(255,255,255,255) 100%);"></div>
-	  Starting in 1996, <a href="http://www.alexa.com/">Alexa Internet</a> has been donating their crawl data to the Internet Archive.  Flowing in every day, these data are added to the <a href="http://web.archive.org/">Wayback Machine</a> after an embargo period.
-	</div>
-	      </div>
-      <div style="display:inline-block;vertical-align:top;width:49%;">
-			<span class="c-logo" style="background-image:url(https://archive.org/services/img/alexa_dg)"></span>
-		<div>Collection: <a style="color:#33f;" href="https://archive.org/details/alexa_dg" target="_new"><span class="wm-title">Alexa Crawls DG</span></a></div>
-		<div style="max-height:75px;overflow:hidden;position:relative;">
-	  <div style="position:absolute;top:0;left:0;width:100%;height:75px;background:linear-gradient(to bottom,rgba(255,255,255,0) 0%,rgba(255,255,255,0) 90%,rgba(255,255,255,255) 100%);"></div>
-	  Crawl data donated by Alexa Internet. This data is currently not publicly accessible
-	</div>
-	      </div>
-    </div>
-    <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center" title="Timestamps for the elements of this page">TIMESTAMPS</div>
-    <div>
-      <div id="wm-capresources" style="margin:0 5px 5px 5px;max-height:250px;overflow-y:scroll !important"></div>
-      <div id="wm-capresources-loading" style="text-align:left;margin:0 20px 5px 5px;display:none"><img src="/static/images/loading.gif" alt="loading" /></div>
-    </div>
-  </div></div></div></div><script type="text/javascript">
-__wm.bt(575,27,25,2,"web","http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c","2001-08-22",1996);
-</script>
-<!-- END WAYBACK TOOLBAR INSERT --><iframe id="playback" src="https://web.archive.org/web/20010822230811if_/http://pubwww.srce.hr:80/hsin/ceoi98/solutions/subtract.c" frameborder="0" style="position:absolute;top:65px;left:0;width:100%;">
-</iframe>
-<script type="text/javascript">
-var margin_top = 65;
-function fitPlayback() {
-  $('#playback').css({
-    height: (window.innerHeight - margin_top) + 'px',
-    top: margin_top
-  });
+
+void write_output(void)
+{
+  int i;
+  int number,counter,temp;
+
+  // writing to "SUBTRACT.OUT"
+
+  fp = fopen("SUBTRACT.OUT","wt");
+
+  // transforming "signs" to sequence of contractions
+  // going from end of "signs", looking for subsequence with minus sign on
+  // the beginning and plus signs on the rest of subsequence
+
+  number = n - 1;
+  counter = 0;
+
+  do
+  {
+    while (signs[number] == -1)
+      --number;
+
+    if (!number)
+      break;
+
+    temp = 0;
+
+    while (signs[number] == 1)
+    {
+      --number;
+      ++temp;
+    }
+
+    for (i = 0;i < temp;++i)
+    {
+      fprintf(fp,"%d\n",number + 1);
+      ++counter;
+    }
+
+    --number;
+  } while (1);
+
+  // other contractions is 1, total number of contractions must be n-1
+
+  for (i = 0;i < n - 1 - counter;++i)
+    fprintf(fp,"1\n");
+
+  fclose(fp);
 }
-fitPlayback();
-$(window).on('resize', function() {
-  fitPlayback(); 
-});
-$('#wm-tb-close').click(function(){
-  margin_top = 0;
-  fitPlayback();
-});
-</script>
-</body>
-</html>
+
+int main(void)
+{
+  read_input();
+
+  solve();
+
+  write_output();
+
+  return 0;
+}
