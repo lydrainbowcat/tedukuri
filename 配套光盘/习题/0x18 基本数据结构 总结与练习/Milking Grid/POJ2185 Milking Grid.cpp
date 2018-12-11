8b28bf12c0fd2d3872ea72108756f424488c371c
lydrainbowcat <hyqjlyd@qq.com>
2018-12-11 23:25:48 +0900

    fix std of POJ2185

--- 配套光盘/习题/0x18 基本数据结构 总结与练习/Milking Grid/POJ2185 Milking Grid.cpp
+++ 配套光盘/习题/0x18 基本数据结构 总结与练习/Milking Grid/POJ2185 Milking Grid.cpp
@@ -2,52 +2,35 @@
 #include <cstdio>
 #include <cstring>
 #include <iostream>
-#define ll long long
+#define ull unsigned long long
 using namespace std;
-const double eps = 0.00000001;
-const int N = 10006;
-char s[N][100], p[N];
-int r0, c0, Next[N];
+const int R = 10006, C = 81, P = 13331;
+int r, c, nxt[R];
+char s[R][C];
+ull H[R];
 
-int get() {
-	int len = strlen(p), i = 0, j = Next[0] = -1;
-	while (i < len) {
-		while (j != -1 && p[i] != p[j]) j = Next[j];
-		Next[++i] = ++j;
-	}
-	return len - Next[len];
-}
-
-int gcd(int a, int b) {
-	return b ? gcd(b, a % b) : a;
+int work(int len) {
+	int i = 0, j = -1;
+	nxt[0] = -1;
+	while (i < len)
+		if (j == -1 || H[i] == H[j]) nxt[++i] = ++j;
+		else j = nxt[j];
+	return len - nxt[len];
 }
 
 int main() {
-	cin >> r0 >> c0;
-	for (int i = 0; i < r0; i++) scanf("%s", s[i]);
-	ll r = 1, c = 1;
-	for (int i = 0; i < r0; i++) {
-		for (int j = 0; j < c0; j++)
-			p[j] = s[i][j];
-		p[c0] = '\0';
-		int k = get();
-		r = k / gcd(r, k) * r;
-		if (r >= c0) {
-			r = c0;
-			break;
-		}
-	}
-	for (int i = 0; i < c0; i++) {
-		for (int j = 0; j < r0; j++)
-			p[j] = s[j][i];
-		p[r0] = '\0';
-		int k = get();
-		c = k / gcd(c, k) * c;
-		if (c >= r0) {
-			c = r0;
-			break;
-		}
-	}
-	cout << c * r << endl;
+	cin >> r >> c;
+	for (int i = 0; i < r; i++) scanf("%s", s[i]);
+	memset(H, 0, sizeof(H));
+	for (int i = 0; i < r; i++)
+		for (int j = 0; j < c; j++)
+			H[i] = H[i] * P + s[i][j];
+	int ans = work(r);
+	memset(H, 0, sizeof(H));
+	for (int i = 0; i < c; i++)
+		for (int j = 0; j < r; j++)
+			H[i] = H[i] * P + s[j][i];
+	ans *= work(c);
+	cout << ans << endl;
 	return 0;
 }
