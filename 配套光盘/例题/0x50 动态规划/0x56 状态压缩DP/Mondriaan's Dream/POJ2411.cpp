bf710b34abbd1641559042bfb421fce1d5bc8f9f
byxiaobai <478322272@qq.com>
2019-08-07 03:57:45 +0800

    Update POJ2411.cpp
    原标程不适合初学者学习

--- 配套光盘/例题/0x50 动态规划/0x56 状态压缩DP/Mondriaan's Dream/POJ2411.cpp
+++ 配套光盘/例题/0x50 动态规划/0x56 状态压缩DP/Mondriaan's Dream/POJ2411.cpp
@@ -1,31 +1,36 @@
-#include<iostream>
 #include<cstdio>
+#include<iostream>
 #include<cstring>
-#include<algorithm>
-#include<queue>
-#include<vector>
 using namespace std;
-int n, m;
-long long f[12][1 << 11];
-bool in_s[1 << 11];
-
-int main()  {
-	while (cin >> n >> m && n) {
-		for (int i = 0; i < 1 << m; i++) {
-			bool cnt = 0, has_odd = 0;
-			for (int j = 0; j < m; j++)
-				if (i >> j & 1) has_odd |= cnt, cnt = 0;
-				else cnt ^= 1;
-			in_s[i] = has_odd | cnt ? 0 : 1;
+long long dp[13][1<<13];
+bool check(int s){
+	int rest=0;
+	while(s){
+		if(s&1)rest+=1;
+		else if(rest&1)return 0;
+		s>>=1;
+	}
+	if(rest&1)return 0;
+	return 1;
+}
+int main(){
+	int n,m;
+	while(~scanf("%d%d",&n,&m)){
+		if(n==0||m==0)return 0;
+		memset(dp,0,sizeof(dp));
+		int tot=(1<<m);
+		for(int i=0;i<tot;i++){
+			if(check(i))dp[1][i]=1;
 		}
-		f[0][0] = 1;
-		for (int i = 1; i <= n; i++)
-			for (int j = 0; j < 1 << m; j++) {
-				f[i][j] = 0;
-				for (int k = 0; k < 1 << m; k++)
-					if ((j&k) == 0 && in_s[j | k])
-						f[i][j] += f[i - 1][k];
+		for(int i=1;i<n;i++){
+			for(int j=0;j<tot;j++){
+				if(!dp[i][j])continue;
+				for(int k=0;k<tot;k++){
+					if((k|j)!=tot-1||(!check(k&j)))continue;
+					dp[i+1][k]+=dp[i][j];
+				}
 			}
-		cout << f[n][0] << endl;
+		}
+		printf("%lld\n",dp[n][tot-1]);
 	}
 }
