5f68101adc9b9bce262677f2a6abdfe926bb34f4
byxiaobai <478322272@qq.com>
2019-11-04 21:02:54 +0800

    Update POJ3171 Cleaning Shifts.cpp
    不应当在a[i].r>=r时直接return。
    测试数据：
    3 1 4
    1 2 1
    4 4 1
    2 4 1

--- 配套光盘/例题/0x50 动态规划/0x58 数据结构优化DP/Cleaning Shifts/POJ3171 Cleaning Shifts.cpp
+++ 配套光盘/例题/0x50 动态规划/0x58 数据结构优化DP/Cleaning Shifts/POJ3171 Cleaning Shifts.cpp
@@ -1,66 +1,70 @@
-//Author:XuHt
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <algorithm>
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
 using namespace std;
-const int N = 100006, INF = 0x3f3f3f3f;
-int n, l, r, f[N];
-struct T {
-	int l, r, x;
-	bool operator < (const T w) const {
-		return r < w.r;
+const int MAXN=4e5,INF=0x3f3f3f3f,root=1;
+struct Node{
+	int l,r,val;
+	bool operator <(Node another)const{
+		return r<another.r;
 	}
-} a[N], t[N<<2];
-
-void build(int p, int l, int r) {
-	t[p].l = l;
-	t[p].r = r;
-	if (l == r) {
-		t[p].x = f[l];
+}tr[MAXN],a[MAXN];
+int f[MAXN];
+void build(int p,int l,int r){
+	tr[p].l=l,tr[p].r=r;
+	if(l==r){
+		tr[p].val=f[l];
 		return;
 	}
-	int mid = (l + r) >> 1;
-	build(p << 1, l, mid);
-	build(p << 1 | 1, mid + 1, r);
-	t[p].x = min(t[p<<1].x, t[p<<1|1].x);
+	int mid=(l+r)>>1;
+	build(p<<1,l,mid);
+	build((p<<1)|1,mid+1,r);
+	tr[p].val=min(tr[p<<1].val,tr[(p<<1)|1].val);
 }
-
-void change(int p, int x, int y) {
-	if (t[p].l == t[p].r) {
-		t[p].x = y;
-		return;
+int query(int p,int ll,int rr){
+	int l=tr[p].l,r=tr[p].r;
+	if(l>=ll&&r<=rr){
+		return tr[p].val;
 	}
-	int mid = (t[p].l + t[p].r) >> 1;
-	if (x <= mid) change(p << 1, x, y);
-	else change(p << 1 | 1, x, y);
-	t[p].x = min(t[p<<1].x, t[p<<1|1].x);
-}
-
-int ask(int p, int l, int r) {
-	if (l <= t[p].l && r >= t[p].r) return t[p].x;
-	int mid = (t[p].l + t[p].r) / 2, ans = INF;
-	if (l <= mid) ans = min(ans, ask(p << 1, l, r));
-	if (r > mid) ans = min(ans, ask(p << 1 | 1, l, r));
+	int mid=(l+r)>>1;
+	int ans=INF;
+	if(ll<=mid)ans=min(ans,query(p<<1,ll,rr));
+	if(rr>mid)ans=min(ans,query((p<<1)|1,ll,rr));
 	return ans;
 }
-
-int main() {
-	cin >> n >> l >> r;
-	for (int i = 1; i <= n; i++)
-		scanf("%d %d %d", &a[i].l, &a[i].r, &a[i].x);
-	sort(a + 1, a + n + 1);
-	memset(f, 0x3f, sizeof(f));
-	f[l] = 0;
-	build(1, l, r);
-	for (int i = 1; i <= n; i++) {
-		f[a[i].r] = min(f[a[i].r], ask(1, a[i].l - 1, a[i].r) + a[i].x);
-		change(1, a[i].r, f[a[i].r]);
-		if (a[i].r >= r) {
-			if (f[a[i].r] == INF) puts("-1");
-			else cout << f[a[i].r] << endl;
-			return 0;
+void change(int p,int x,int val){
+	int l=tr[p].l,r=tr[p].r;
+	if(l==r){
+		tr[p].val=val;
+		return;
+	}
+	int mid=(l+r)>>1;
+	if(x<=mid)change(p<<1,x,val);
+	else if(x>mid)change((p<<1)|1,x,val);
+	tr[p].val=min(tr[p<<1].val,tr[(p<<1)|1].val);
+}
+int main(){
+	int n,l,r;
+	scanf("%d%d%d",&n,&l,&r);
+	l+=2,r+=2;
+	for(int i=1;i<=n;i++){
+		scanf("%d%d%d",&a[i].l,&a[i].r,&a[i].val);
+		a[i].l+=2,a[i].r+=2;
+	}
+	sort(a+1,a+1+n);
+	memset(f,0x3f,sizeof(f));
+	f[l-1]=0;
+	build(root,l-1,r);
+	int ans=INF;
+	for(int i=1;i<=n;i++){
+		f[a[i].r]=min(f[a[i].r],query(root,a[i].l-1,a[i].r)+a[i].val);
+		change(root,a[i].r,f[a[i].r]);
+		if(a[i].r>=r){
+			ans=min(ans,f[a[i].r]);
 		}
 	}
+	if(ans==INF)printf("-1\n");
+	else printf("%d\n",ans);
 	return 0;
 }
