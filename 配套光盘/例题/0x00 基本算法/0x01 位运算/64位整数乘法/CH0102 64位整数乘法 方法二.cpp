1de9ed229f7d6f88fb9bd2547dcfc519c9630ebb
lydrainbowcat <hyqjlyd@qq.com>
2020-05-16 13:47:46 +0900

    fix 64bit mul long double algo

--- 配套光盘/例题/0x00 基本算法/0x01 位运算/64位整数乘法/CH0102 64位整数乘法 方法二.cpp
+++ 配套光盘/例题/0x00 基本算法/0x01 位运算/64位整数乘法/CH0102 64位整数乘法 方法二.cpp
@@ -1,19 +1,20 @@
-//Author:XuHt
-#include <iostream>
-#define ll long long
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
 using namespace std;
+long long a, b, p;
 
-ll mul(ll a, ll b, ll p) {
-	a %= p;
-	b %= p;
-	ll c = (long double)a * b / p;
-	ll ans = a * b - c * p;
-	return (ans % p + p) % p;
+typedef unsigned long long ull;
+ull mul(ull a, ull b, ull p) {
+	a %= p, b %= p;  // 当a,b一定在0~p之间时，此行不必要
+	ull c = (long double)a * b / p;
+	ull ans1 = a * b, ans2 = c * p;
+	if (ans1 < ans2) return ans2 - ans1;
+	return ans1 - ans2;
 }
 
 int main() {
-	ll a, b, p;
 	cin >> a >> b >> p;
 	cout << mul(a, b, p) << endl;
-	return 0;
 }
--- 配套光盘/正文包含的程序片段/0x01 bit.cpp
+++ 配套光盘/正文包含的程序片段/0x01 bit.cpp
@@ -19,13 +19,13 @@ long long mul(long long a, long long b, long long p) {
 }
 
 // 64位整数乘法的long double算法
-long long mul(long long a, long long b, long long p) {
-	a %= p, b %= p; // 当a,b一定在0~p之间时，此行不必要。
-	long long c = (long double)a * b / p;
-	long long ans = a * b - c * p;
-	if (ans < 0) ans += p;
-	else if (ans >= p) ans -= p;
-	return ans;
+typedef unsigned long long ull;
+ull mul(ull a, ull b, ull p) {
+	a %= p, b %= p;  // 当a,b一定在0~p之间时，此行不必要
+	ull c = (long double)a * b / p;
+	ull ans1 = a * b, ans2 = c * p;
+	if (ans1 < ans2) return ans2 - ans1;
+	return ans1 - ans2;
 }
 
 
