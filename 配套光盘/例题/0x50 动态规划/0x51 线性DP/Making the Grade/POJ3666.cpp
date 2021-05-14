bf067b7c7c58a263c7816480d58785ae19be4d4d
lydrainbowcat <hyqjlyd@qq.com>
2021-05-14 22:43:59 +0900

    fix POJ3666.cpp

--- 配套光盘/例题/0x50 动态规划/0x51 线性DP/Making the Grade/POJ3666.cpp
+++ 配套光盘/例题/0x50 动态规划/0x51 线性DP/Making the Grade/POJ3666.cpp
@@ -1,33 +1,47 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
-const int SIZE = 2010;
-int a[SIZE], c[SIZE], nums[SIZE];
-int f[SIZE][SIZE];
+
+const int N = 2010, INF = 0x3f3f3f3f;
+
 int n;
-int main() {
-	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		scanf("%d", &a[i]);
-		nums[i] = a[i];
-	}
-	sort(nums+1, nums+n+1);
-	int m = unique(nums+1, nums+n+1)-nums-1;
-	for (int i = 1; i <= n; i++)
-		c[i] = lower_bound(nums+1, nums+m+1, a[i]) - nums;
-	memset(f, 0x3f, sizeof(f));
-	f[0][0] = 0;
-	for (int i = 1; i <= n; i++) {
-		int temp = f[i-1][0];
-		for (int j = 1; j <= m; j++) {
-			temp = min(temp, f[i-1][j]);
-			f[i][j] = temp + abs(a[i] - nums[j]);
-		}
-	}
-	int ans = 1 << 30;
-	for (int i = 1; i <= m; i++)
-		ans = min(ans, f[n][i]);
-	cout << ans << endl;
+int a[N], b[N];
+int f[N][N];
+
+int work()
+{
+    for (int i = 1; i <= n; i ++ ) b[i] = a[i];
+    sort(b + 1, b + n + 1);  // 离散化，用索引代表数值进行DP
+
+    for (int i = 1; i <= n; i ++ )
+    {
+        int minv = INF;
+        for (int j = 1; j <= n; j ++ )
+        {
+            minv = min(minv, f[i - 1][j]);
+            f[i][j] = minv + abs(a[i] - b[j]);
+        }
+    }
+
+    int res = INF;
+    for (int i = 1; i <= n; i ++ ) res = min(res, f[n][i]);
+
+    return res;
+}
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i ++ ) scanf("%d", &a[i]);
+
+    int res = work();
+    reverse(a + 1, a + n + 1);
+    res = min(res, work());
+
+    printf("%d\n", res);
+
+    return 0;
 }
