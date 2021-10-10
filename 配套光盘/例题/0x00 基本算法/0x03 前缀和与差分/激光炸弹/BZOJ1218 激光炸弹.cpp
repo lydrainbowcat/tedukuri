2f43133cad675065a57fba105ccf9c32ec9533d3
huyiyun2020 <77782398+huyiyun2020@users.noreply.github.com>
2021-10-10 21:22:05 +0800

    Update BZOJ1218 激光炸弹.cpp

--- 配套光盘/例题/0x00 基本算法/0x03 前缀和与差分/激光炸弹/BZOJ1218 激光炸弹.cpp
+++ 配套光盘/例题/0x00 基本算法/0x03 前缀和与差分/激光炸弹/BZOJ1218 激光炸弹.cpp
@@ -22,12 +22,17 @@ int main() {
 			else if (!j) s[i][j] += s[i-1][j];
 			else s[i][j] += s[i-1][j] + s[i][j-1] - s[i-1][j-1];
 	int ans = 0;
-	for (int i = r - 1; i <= 5000; i++)
-		for (int j = r - 1; j <= 5000; j++)
-			if (i == r - 1 && j == r - 1) ans = max(ans, s[i][j]);
-			else if (i == r - 1) ans = max(ans, s[i][j] - s[i][j-r]);
-			else if (j == r - 1) ans = max(ans, s[i][j] - s[i-r][j]);
-			else ans = max(ans, s[i][j] - s[i-r][j] - s[i][j-r] + s[i-r][j-r]);
+	if(r<5001){
+		for (int i = r - 1; i <= 5000; i++)
+			for (int j = r - 1; j <= 5000; j++)
+				if (i == r - 1 && j == r - 1) ans = max(ans, s[i][j]);
+				else if (i == r - 1) ans = max(ans, s[i][j] - s[i][j-r]);
+				else if (j == r - 1) ans = max(ans, s[i][j] - s[i-r][j]);
+				else ans = max(ans, s[i][j] - s[i-r][j] - s[i][j-r] + s[i-r][j-r]);
+	}
+	else{	//r>=5001，可以覆盖所有区域
+		ans=s[5000][5000];
+	}
 	cout << ans << endl;
 	return 0;
 }
