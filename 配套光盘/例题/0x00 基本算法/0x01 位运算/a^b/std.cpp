41030d4bc15c73c9c62460cb77649695dc4e5cc2
huyiyun2020 <77782398+huyiyun2020@users.noreply.github.com>
2021-11-01 11:42:15 +0800

    Update std.cpp

--- 配套光盘/例题/0x00 基本算法/0x01 位运算/a^b/std.cpp
+++ 配套光盘/例题/0x00 基本算法/0x01 位运算/a^b/std.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int a, b, p;
 
 int power(int a, int b, int p) { // calculate (a ^ b) mod p
-	int ans = 1 % p;
+	int ans = 1 % p;	//因为p可能为1，此时ans==0，其实我认为直接用“if(p==1) return 0;”更好，不用计算下面的
 	for (; b; b >>= 1) {
 		if (b & 1) ans = (long long)ans * a % p;
 		a = (long long)a * a % p;
